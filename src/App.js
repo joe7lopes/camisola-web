df4163f12deab8e6ee4e94d8872420095db02468
Joao Oliveira <j.andre.oliveira@gmail.com>
2020-01-16 04:10:42 +0100

    refactored store to its own file.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer from './reducers';
 import { Routes } from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss';
-import rootSaga from './sagas';
+import store from './store';
 
-const App = () => {
-  const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(sagaMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
-  );
-
-  sagaMiddleware.run(rootSaga);
-
-  return (
+const App = () => (
     <Provider store={store}>
       <Routes/>
     </Provider>
-  );
-};
+);
 
 export default App;
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;

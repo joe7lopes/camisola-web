8ba3ceae8ea116fdd60d778e75b91a5370391683
Joao Oliveira <j.andre.oliveira@gmail.com>
2020-02-06 07:41:15 +0100

    composed order summary component

--- src/components/account/OrderSummary.tsx
+++ src/components/account/OrderSummary.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
-function OrderSummary() {
+const OrderSummary = () => {
   return (
     <div>
-      <h1>OrderSummary</h1>
+      <h3>Encomenda #8976 criada a Janeiro 16, 2020 e está actualmente no estado Cancelada.</h3>
+      <div className="c-container">
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Preço</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Thumbnail + product description</td>
+              <td>{`${20} €`}</td>
+              <td>{`${30} €`}</td>
+            </tr>
+          </tbody>
+        </Table>
+        <Table striped bordered hover>
+          <tbody>
+            <tr>
+              <th>Sub-total</th>
+              <td>{5} €</td>
+            </tr>
+            <tr>
+              <th>Envio</th>
+              <td>Taxa fixa: 5€ protes de envio</td>
+            </tr>
+            <tr>
+              <th>Total da compra</th>
+              <td>{10} €</td>
+            </tr>
+          </tbody>
+        </Table>
+        <div>
+          morada de envio ...
+        </div>
+      </div>
     </div>
   );
 }

6a4d427692bd07fb98dfdbf4a85068a2e6c83189
Joao <joao.oliveira@branchspace.com>
2021-02-14 16:31:08 +0100

    added facebook home page reviews link

--- src/components/home/facebook/FacebookReviews.tsx
+++ src/components/home/facebook/FacebookReviews.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Paper } from '@material-ui/core';
+import StarRoundedIcon from '@material-ui/icons/StarRounded';
 import { IRootState } from '../../../types';
 
 const FacebookReviews = () => {
@@ -19,11 +20,20 @@ const FacebookReviews = () => {
     return `${date.getHours()}:${date.getMinutes()} ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
   };
 
+  const facebookLink = (text: string) => (
+        <a href="https://www.facebook.com/Camisola10-Geral-105428654698416"
+           target="_blank"
+           rel="noopener noreferrer">
+            {text}
+        </a>
+  );
+
   return (
         <div>
-            <h4 className="m-t-md">
-                Opinião dos clientes
-            </h4>
+            <h5 className="m-t-md">
+                Opinião dos Clientes no <span>{facebookLink('Facebook')}</span>
+                <div className="c-text-sm">Rating 5 de 5 <StarRoundedIcon/> {facebookLink('veja aqui...')}</div>
+            </h5>
             {facebookReviews.reviews.map((review, id) => (
                 <Paper elevation={3} key={id} className="m-t-md p-xs c-text-sm">
                     {review.review_text}
